d59af8f58e54b4cbd2c12964a9226085f534a8a4
Kuanysh <kuanysh01012@gmail.com>
2025-06-19 11:51:50 +0500

    jwt auth commented

--- eTanuReincarnationAPI/settings.py
+++ eTanuReincarnationAPI/settings.py
@@ -62,7 +62,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'metadata.middleware.RequestTimeMiddleware',
-    'metadata.middleware.JWTAuthMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 ]
 
